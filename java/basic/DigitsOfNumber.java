21de578a772a49a906ce73886bdd4e6a8a7d17ea
nilay_d <niayniay99@gmail.com>
2023-06-27 15:19:10 +0530

    By Nilay

--- java/basic/DigitsOfNumber.java
+++ java/basic/DigitsOfNumber.java
@@ -1,11 +1,22 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class DigitsOfNumber{
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number");
+        System.out.println("Enter any positive integer : ");
         int A = sc.nextInt();
-        System.out.println(A);
+
+        ArrayList<Integer> digitsList = new ArrayList<>();
+        while( A > 0){
+            int unitDigit = A % 10;
+            digitsList.add(0, unitDigit);
+            A = A/10;
+        }
+
+        System.out.println("The digits of given number : ");
+        for(int digit : digitsList)
+             System.out.print(digit+" ");
         sc.close();
     }
 }
