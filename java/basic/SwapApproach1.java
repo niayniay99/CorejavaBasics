8faebb8de4af9609dd4aea878623f9e661ad471c
Nilay Doshi <59107722+niayniay99@users.noreply.github.com>
2023-06-27 11:15:48 +0530

    Add files via upload

---
+++ java/basic/SwapApproach1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SwapApproach1
+{
+	public static void main(String[] args)
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter value for a : ");
+	int a = sc.nextInt();
+	System.out.println("Enter value for b : ");
+	int b = sc.nextInt();
+	
+	System.out.println("Before swapping  a&b : ");
+	System.out.println("a = "+a+", b = "+b);
+	
+	//using operators
+	a = a+b;
+	b = a-b;
+	a = a-b;
+	
+	System.out.println("After swapping  a&b : ");
+	System.out.println("a = "+a+", b = "+b);
+	sc.close();
+	}
+}
---
+++ java/basic/SwapApproach3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SwapApproach3
+{
+	public static void main(String[] args)
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter value for a : ");
+	int a = sc.nextInt();
+	System.out.println("Enter value for b : ");
+	int b = sc.nextInt();
+	
+	System.out.println("Before swapping  a&b : ");
+	System.out.println("a = "+a+", b = "+b);
+	
+	a = a^b;
+	b = a^b;
+	a = a^b;
+	
+	System.out.println("After swapping  a&b : ");
+	System.out.println("a = "+a+", b = "+b);
+	
+	}
+}
