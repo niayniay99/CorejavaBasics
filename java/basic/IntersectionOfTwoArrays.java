789f847fdf195aaf81f0562ddbd0985d1b800cee
nilay_d <niayniay99@gmail.com>
2023-07-01 23:22:00 +0530

    By Nilay

---
+++ java/basic/IntersectionOfTwoArrays.java
@@ -0,0 +1,16 @@
+public class IntersectionOfTwoArrays {
+    public static void main(String[] args) {
+        int array1[] = {44,49,39,26,67,69};
+        int array2[] = {44,39,27,68,69,135};
+        
+        System.out.println("Given array 1 is ");
+        for(int v :array1)
+        System.out.println(v+"");
+
+        System.out.println("Given array 2 is ");
+        for(int v :array2)
+        System.out.println(v+"");
+        
+        
+    }
+}
