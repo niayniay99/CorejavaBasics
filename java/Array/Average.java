68be539ce7ca3992f3ec4c2e260e649639f82f64
nilay_d <niayniay99@gmail.com>
2023-07-07 23:14:45 +0530

    Arrays Revision By Nilay

--- java/Array/Average.java
+++ java/Array/Average.java
@@ -6,7 +6,7 @@ public class Average {
     {
         int array[] = {10,20,30,40,50,60,70,80,90,100};
         int n = array.length;
-        int sum =0;
+        int sum = 0;
 
         for(int v : array)
             sum += v;
@@ -14,6 +14,6 @@ public class Average {
         System.out.println("The given array is : ");
         for(int v : array)
         System.out.print(v+" ");
-        System.out.println("\n The Average of  GIven array is : "+average);
+        System.out.println("\n The Average of  Given array is : "+average);
     }
 }
---
+++ java/Array/UnionOfSortedArrays.java
@@ -0,0 +1,15 @@
+package java.Array;
+
+import java.util.ArrayList;
+
+public class UnionOfSortedArrays {
+    public static void main(String[] args) {
+        int array1[] = {1,2,3,4,5,6};
+        int array2[] = {2,4,6,8,10};
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int i = 0, j = 0;
+        
+    }
+}
