4195dc7303ecb54264ffdec1980e1c2905d5ce11
nilay_d <niayniay99@gmail.com>
2023-07-09 00:26:32 +0530

    String concept revision

---
+++ java/strings/Palindrome.java
@@ -0,0 +1,10 @@
+package java.strings;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String word = "malyalam";
+
+        System.out.println("The Given String \" "+word+"\" ");
+        
+    }
+}
