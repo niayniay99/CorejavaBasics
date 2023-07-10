dfd67139beaf50c9ac39bbd70e1710d8713510e6
nilay_d <niayniay99@gmail.com>
2023-07-10 22:42:13 +0530

    Strings revision By Nilay

--- java/strings/Palindrome.java
+++ java/strings/Palindrome.java
@@ -5,6 +5,19 @@ public class Palindrome {
         String word = "malyalam";
 
         System.out.println("The Given String \" "+word+"\" ");
-        
+        if(checkPalindrome(word))
+        System.out.println("Yes the given string is a palindrome");
+        else
+            System.out.println("No, The given string is NOT a Palindrome.");
+
+    }
+
+    private static boolean checkPalindrome(String str)
+    {
+        int strlen = str.length();
+        for(int i = 0; i < strlen/2 ; i++)
+            if(str.charAt(i) != str.charAt(strlen - i -1))
+                return false;
+            return true;
     }
 }
