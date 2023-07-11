d80594e4de4f5242cbd2a6a684c122eba5757163
nilay_d <niayniay99@gmail.com>
2023-07-11 08:52:31 +0530

    By Nilay

--- java/strings/Palindrome.java
+++ java/strings/Palindrome.java
@@ -1,12 +1,13 @@
 package java.strings;
 
+
 public class Palindrome {
     public static void main(String[] args) {
-        String word = "malyalam";
+        String word = "malayalam";
 
         System.out.println("The Given String \" "+word+"\" ");
         if(checkPalindrome(word))
-        System.out.println("Yes the given string is a palindrome");
+        System.out.println("Yes, the given string is a palindrome");
         else
             System.out.println("No, The given string is NOT a Palindrome.");
 
