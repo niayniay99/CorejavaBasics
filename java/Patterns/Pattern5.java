ec0dd65a5b3dd22bfe2c098e0458008d99cbfc46
nilay_d <niayniay99@gmail.com>
2023-07-23 22:20:00 +0530

    By Nilay

---
+++ java/Patterns/Pattern5.java
@@ -0,0 +1,26 @@
+package java.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of Rows for Floyd's Triangle to print");
+        int rows = Integer.parseInt(sc.nextLine().trim());
+
+
+        // logic
+        int value = 0;        
+        for(int i = 1;i<=rows;i++)
+        {
+            value +=1;
+            for(int j = 1;j<i+2;j++)
+            {
+                System.out.print(value+" ");
+                
+             } System.out.println();
+            
+            sc.close();
+        }
+    }
+}
