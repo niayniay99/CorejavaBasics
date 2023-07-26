b9f0d0bef3557499df2061cddd44f74c7d3cd932
nilay_d <niayniay99@gmail.com>
2023-07-26 23:05:16 +0530

    Patterns

---
+++ java/Patterns/Pattern7.java
@@ -0,0 +1,28 @@
+package java.Patterns;
+
+
+import java.util.Scanner;
+
+public class Pattern7 {
+  public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of Rows for Floyd's Triangle to print");
+        int rows = Integer.parseInt(sc.nextLine().trim());
+
+
+        // logic
+      //  int value = rows;        
+        for(int i = rows;i>=1;i--)
+        {
+            //value +=1;
+           
+            for(int j = 1;j<=i;j++)
+            {
+                System.out.print(j+" ");         
+            }
+        System.out.println();
+          //  value--;
+            sc.close();
+        }
+    }      
+}
